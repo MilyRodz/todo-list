800bab86175dbdca9e58613420871d45adb720b3
Mily <milycrn26@gmail.com>
2021-03-09 14:16:43 -0300

    fix delete function

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,11 +6,11 @@ const TaskList = ({list, deleteTask}) => {
             <ul className="list-group list-group-flush task-area"> 
              {
                 list.map( (item) => {
-                    return <li href="/#" className="list-group-item list-group-item-action rounded-0 border text-left" 
+                    return <li className="list-group-item list-group-item-action rounded-0 border p-3 text-left button-appear list-group-with-button" 
                         key={item.label} item={item}>
                         {item.label}
                         <button type="button" className="delete-button text-right"
-                            onClick={ () => deleteTask(item.id) }
+                            onClick={ () => deleteTask(item.label) }
                             >&times;</button>
                         </li>
                 })
